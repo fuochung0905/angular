3a97abcf70dd900ee5d4bf5182184f59f2ded283
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2024-03-25 22:32:55 +0700

    update

--- src/app/admin/component/postproduct/postproduct.component.ts
+++ src/app/admin/component/postproduct/postproduct.component.ts
@@ -10,21 +10,14 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class PostproductComponent {
 
+  productForm!:FormGroup;
   listOfCategories:any=[];
-  selectedFile: File | null | undefined;
-  imagePreView: String | ArrayBuffer | null | undefined;
+  selectedFile!: File | '';
+  imagePreView!: String | ArrayBuffer | null ;
 
-  productDto:any={
-    categoryId:'',
-    productName:'',
-    description:'',
-    importPrice:'',
-    exportPrice:'',
-    quantity:'',
-    image:null
 
-  };
   constructor(private adminService:AdminService,
+    private fb :FormBuilder,
     private router:Router){
 
   };
@@ -43,6 +36,14 @@ PreViewImage(){
   }
 };
   ngOnInit(){
+    this.productForm=this.fb.group({
+      categoryId:[null],
+      productName:[null],
+      description:[null],
+      importPrice:[null],
+      exportPrice:[null],
+      quantity:[null]
+    })
     this.getAllCategories();
   };
   getAllCategories(){
@@ -51,9 +52,32 @@ PreViewImage(){
      this.listOfCategories=res;
     })
   };
-  addProduct(){
-    this.productDto.image=this.selectedFile;
-    this.adminService.addProduct(this.productDto).subscribe(
+  addProduct():void{
+    if(this.productForm.valid){
+      const formData :FormData=new FormData();
+      formData.append('image',this.selectedFile);
+      formData.append('categoryId',this.productForm.get('categoryId')?.value);
+      formData.append('productName',this.productForm.get('productName')?.value);
+      formData.append('description',this.productForm.get('description')?.value);
+      formData.append('importPrice',this.productForm.get('importPrice')?.value);
+      formData.append('exportPrice',this.productForm.get('exportPrice')?.value);
+      formData.append('quantity',this.productForm.get('quantity')?.value);
+this.adminService.addProduct(formData).subscribe(
+  (res)=>{
+      console.log('thêm thành công',res);
+  },
+  (error)=>{
+    console.log('thêm thất bại',error);
+  }
+)
+    }
+    else{
+      for(const i in this.productForm.controls){
+        this.productForm.controls[i].markAsDirty();
+        this.productForm.controls[i].updateValueAndValidity();
+      }
+    }
+    this.adminService.addProduct(this.productForm.value).subscribe(
       (response) => {
         console.log('Product added successfully:', response);
         // Reset the form after successfully adding the product
